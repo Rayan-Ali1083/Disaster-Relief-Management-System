75ab011c3c75f553847f4f2768c012b3f2f32684
maarij0160 <maarijamir1@gmail.com>
2022-11-22 17:48:31 +0500

    Latest

--- client/src/frontend/Login/Login_page.js
+++ client/src/frontend/Login/Login_page.js
@@ -1,14 +1,35 @@
 import './Login_page.css'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, redirect } from 'react-router-dom'
 import person from './images/person.png'
 import React, {useState} from 'react'
+import Axios from 'axios'
 
 const Login_page = () => {
 
    const [user, setUser] = useState({
     email: "", password:""
    });
+   const[loginStatus, setLoginStatus] = useState("");
 
+   const authenticate = ()=>{
+
+        if(loginStatus){
+          <Link to='Signup.js' activeClassName="active">{loginStatus}</Link>
+        }
+   };
+  
+   const login = ()=>{
+    Axios.post("http://localhost:3001/api/login",{username:user.email,password:user.password,}).then((response)=>{
+      if(response.data.message){
+       //setLoginStatus(response.data.message)
+        alert(response.data.message);
+      }else{
+        setLoginStatus('true')
+        alert("Logged In");
+        //authenticate()
+      }
+    });
+  };
    let name, value;
 
    const handleInputs = (e) =>{
@@ -34,6 +55,7 @@ const Login_page = () => {
             <input className="form-control" id="textarea" name='password' value={user.password} 
             onChange={handleInputs} placeholder='***********'></input>
           </div>
+          <button type="button" onClick={login} className="btn btn-primary" style={{"float":"centre"}}>Login</button>
           <div id="text"><b>New user? Sign up</b>
           <NavLink to='Signup.js'><b> here</b></NavLink>
           </div>
@@ -42,6 +64,7 @@ const Login_page = () => {
           <button type="button" className="btn btn-primary" style={{"marginLeft": "85%", "marginTop": "3%"}}>Admin</button>
         </NavLink>
 
+    <h1>{loginStatus}</h1>
       </div>
     </>
   )

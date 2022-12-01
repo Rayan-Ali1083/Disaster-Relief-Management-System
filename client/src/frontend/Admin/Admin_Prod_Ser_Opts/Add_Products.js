fd06866b3cc1fb87091cbdbec5cf94da0441efce
maarij0160 <maarijamir1@gmail.com>
2022-12-02 00:17:00 +0500

    tip top

--- client/src/frontend/Admin/Admin_Prod_Ser_Opts/Add_Products.js
+++ client/src/frontend/Admin/Admin_Prod_Ser_Opts/Add_Products.js
@@ -1,6 +1,32 @@
-import React from 'react'
-
+import React, {useState,useEffect} from 'react'
+import Axios from 'axios'
 function Add_Products() {
+
+
+  useEffect(()=>{
+    
+  })
+  const [newProduct,setnewProduct] = useState({
+    product_name: "", product_cate: "" 
+  });
+
+  let name, value;
+
+  const handleIn = (e) =>{
+    name = e.target.name;
+    value = e.target.value;
+    setnewProduct({...newProduct, [name]:value});
+   
+   }
+   const addProduct = ()=>{
+    Axios.post("http://localhost:3001/api/addProd",{prod:newProduct}).then((resultx)=>{
+      if(resultx.data.message){
+        alert(resultx.data.message);
+      }else{
+        alert(resultx.data.message1);
+      }
+    })
+  };
   return (
     <>
         <div className="card" style={{"margin": "auto", "width": "100%","border": "10px solid green","padding":"5%"}}>
@@ -17,10 +43,10 @@ function Add_Products() {
         </thead>
             <tbody>
                 <tr>
-                    <td><input placeholder='Example 001'></input></td>
-                    <td><input placeholder='Example Milk'></input></td>
-                    <td><input placeholder='Example Food'></input></td>
-                    <td><button type="button" style={{"background":"#89bd79", "borderRadius":"5px", "borderStyle":"none"}}>ADD</button></td>
+                    <td><input  placeholder='Example 001'></input></td>
+                    <td><input value={newProduct.product_name} name='product_name' onChange={handleIn} placeholder='Example Milk'></input></td>
+                    <td><input value={newProduct.product_cate} name='product_cate'onChange={handleIn} placeholder='Example Food'></input></td>
+                    <td><button type="button" onClick={addProduct} style={{"background":"#89bd79", "borderRadius":"5px", "borderStyle":"none"}}>ADD</button></td>
 
                 </tr>
             </tbody>

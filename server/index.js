f7b15779ebb6b9fa1818b2452ed97ecb4a6230eb
maarij0160 <maarijamir1@gmail.com>
2022-12-03 15:00:58 +0500

    nou

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt');
 const { response } = require('express');
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
+const mom = require('moment')
 
 const saltRounds = 10
 
@@ -15,8 +16,8 @@ const db = mysql.createPool({
 
     host:'localhost',
     user: 'root',
-    password: 'fast123',
-    database: 'drwms'
+    password: 'fast',
+    database: 'dbtest'
 
 });
 

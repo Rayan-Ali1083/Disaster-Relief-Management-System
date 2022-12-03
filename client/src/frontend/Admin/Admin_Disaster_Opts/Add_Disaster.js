516c4b032409793cdc53754df66a2e01d8bf07bd
maarij0160 <maarijamir1@gmail.com>
2022-12-04 01:04:27 +0500

    indeed

--- client/src/frontend/Admin/Admin_Disaster_Opts/Add_Disaster.js
+++ client/src/frontend/Admin/Admin_Disaster_Opts/Add_Disaster.js
@@ -1,5 +1,44 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import Axios from 'axios'
 function Add_Disaster() {
+
+
+
+  const [newDisaster,setnewDisaster] = useState({
+    dis_name: "", date: "",dis_type: "" 
+  });
+  const [disastertype,Setdisastertype] = useState([]);
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/api/disastercateg").then((response)=>{
+      Setdisastertype(response.data)
+      console.log(response)
+
+    })
+
+  },[])
+
+
+  let name, value;
+
+  const handleDisInputs = (e) =>{
+    name = e.target.name;
+    value = e.target.value;
+    setnewDisaster({...newDisaster, [name]:value});
+   
+   }
+
+
+   const addDis = ()=>{
+    Axios.post("http://localhost:3001/api/addDisaster",{dis:newDisaster}).then((resultx)=>{
+      if(resultx.data.message){
+        alert(resultx.data.message);
+      }else{
+        alert(resultx.data.message1);
+      }
+    })
+  };
+
+
   return (
    <>
         <div className="card" style={{"margin": "auto", "width": "100%","border": "10px solid green","padding":"5%"}}>
@@ -16,17 +55,19 @@ function Add_Disaster() {
   <tbody>
  
     <tr>
-        <td>  <input type="text" className="form-control" placeholder="Disaster Name" aria-label="Username"></input>
+        <td>  <input type="text" className="form-control" value={newDisaster.dis_name} name= 'dis_name' onChange={handleDisInputs} placeholder="Disaster Name" aria-label="Username"></input>
 </td>
-        <td>  <input type="date"></input></td>
-        <td><div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Disaster Type</button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item">Action</a></li>
-        </ul>
-        </div></td>
+        <td>  <input type="date" value={newDisaster.date} name='date' onChange={handleDisInputs}  ></input></td>
+        <td>
+                <select class="form-select" name='dis_type' value={newDisaster.dis_type} onChange={handleDisInputs} aria-label="Default select example">
+                <option selected>Disaster type</option>
+              {disastertype.map((val)=>(
+        <option >{val.disaster_type}</option> 
+              ))}
+                </select>
+</td>
 
-        <button type="button" style={{"background":"#89bd79", "borderRadius":"5px", "borderStyle":"none"}}>Add</button>
+        <button type="button" onClick={addDis} style={{"background":"#89bd79", "borderRadius":"5px", "borderStyle":"none"}}>Add</button>
     </tr>
    
   </tbody>
